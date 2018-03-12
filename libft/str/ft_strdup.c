7bc52a64c8c5ee055b2fa023e24f011170be6927
Clementine PIRLOT <cpirlot@e1r11p12.42.fr>
2018-03-12 14:49:48 +0100

    refactor strdup

--- libft/str/ft_strdup.c
+++ libft/str/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 07:31:48 by cpirlot           #+#    #+#             */
-/*   Updated: 2017/11/08 07:32:51 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/03/12 14:48:58 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,8 @@ char	*ft_strdup(const char *str)
 {
 	char	*dest;
 
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
-	if (dest)
-	{
-		ft_strcpy(dest, str);
-		return (dest);
-	}
-	else
+	if (!(dest = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1))))
 		return (NULL);
+	ft_strcpy(dest, str);
+	return (dest);
 }
